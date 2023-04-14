dbfc66e7f95d071b111beca9cbcbc59bddac0365
westy12dan <westy12dan@gmail.com>
2023-04-14 10:16:40 +0800

    Start bundling

--- src/lib/render.ts
+++ src/lib/render.ts
@@ -1,8 +1,53 @@
-import { ComponentType } from '@/lib/types.ts';
+import { ComponentType, isComponentType } from '@/lib/types.ts';
 import { VNode } from '@/lib/jsx-runtime';
 import { DOMParser, Element, HTMLDocument } from 'deno-dom';
+import * as esbuild from 'esbuild';
 
 const TEMPLATE = await Deno.readTextFile('./src/index.html');
+
+interface Page {
+    component: ComponentType;
+    file: string;
+}
+
+const rawRuntime = await Deno.readTextFile(`./src/lib/jsx-runtime.ts`);
+export const runtime = (await esbuild.transform(rawRuntime, {
+    loader: 'ts',
+    format: 'esm',
+})).code;
+
+const pageMap: Record<string, Page> = {};
+for (const dirEntry of Deno.readDirSync('./src/pages')) {
+    if (dirEntry.isFile) {
+        const raw = await Deno.readTextFile(`./src/pages/${dirEntry.name}`);
+        const res = await esbuild.transform(raw, {
+            loader: 'tsx',
+            format: 'esm',
+            jsx: 'automatic',
+            jsxImportSource: './lib',
+        });
+        if (res.warnings.length) console.warn(res.warnings);
+
+        const module = await import(`../pages/${dirEntry.name}`);
+        if (!isComponentType(module.default)) {
+            throw new Error(`Module ${dirEntry.name} does not default export a valid component`);
+        }
+
+        let pathName = dirEntry.name.split('.')[0];
+        if (pathName === 'index') pathName = '/';
+        pageMap[pathName] = {
+            component: module.default,
+            file: res.code,
+        };
+    }
+}
+esbuild.stop();
+
+function getComponentForPath(path: string): Page {
+    const page = pageMap[path] || pageMap['404'];
+    if (!page) throw new Error(`No 404 page found`);
+    return page;
+}
 
 function createNode(document: HTMLDocument, vnode: VNode<keyof HTMLElementTagNameMap>): Element {
     const children = vnode.attributes.children;
@@ -31,13 +76,19 @@ function createNode(document: HTMLDocument, vnode: VNode<keyof HTMLElementTagNam
     return el;
 }
 
-export function render(component: ComponentType): string {
-    const vnode = component();
+export function render(url: URL): string {
+    const page = getComponentForPath(url.pathname);
+    const vnode = page.component();
 
     const document = new DOMParser().parseFromString(TEMPLATE, 'text/html');
     if (!document?.documentElement) throw new Error('Failed to parse document template');
 
     document.body?.appendChild(createNode(document, vnode));
+
+    const bundle = document.createElement('script');
+    bundle.setAttribute('type', 'module');
+    bundle.innerHTML = page.file;
+    document.body?.appendChild(bundle);
 
     return document.documentElement.outerHTML;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,23 @@
 import { serve } from 'std/http/server.ts';
-import { ComponentType, isComponentType } from '@/lib/types.ts';
-import { render } from '@/lib/render.ts';
-
-const pageMap: Record<string, ComponentType> = {};
-for (const dirEntry of Deno.readDirSync('./src/pages')) {
-    if (dirEntry.isFile) {
-        const module = await import(`./pages/${dirEntry.name}`);
-        if (!isComponentType(module.default)) {
-            throw new Error(`Module ${dirEntry.name} does not default export a valid component`);
-        }
-        let pathName = dirEntry.name.split('.')[0];
-        if (pathName === 'index') pathName = '/';
-        pageMap[pathName] = module.default;
-    }
-}
-
-function getComponentForPath(path: string): ComponentType {
-    const page = pageMap[path] || pageMap['404'];
-    if (!page) throw new Error(`No 404 page found`);
-    return page;
-}
+import { render, runtime } from '@/lib/render.ts';
 
 function handler(req: Request): Response {
+    const url = new URL(req.url);
+
+    console.log(url.pathname);
+
     if (req.method !== 'GET') {
         return Response.json({ error: 'Method not allowed' }, { status: 405 });
     }
 
-    const url = new URL(req.url);
-    const component = getComponentForPath(url.pathname);
-    const html = render(component);
+    switch (url.pathname) {
+        case '/lib/jsx-runtime':
+            return new Response(new TextEncoder().encode(runtime), {
+                headers: { 'Content-Type': 'application/javascript' },
+            });
+    }
 
+    const html = render(url);
     return new Response(new TextEncoder().encode(html));
 }
 
