a7d3cf8839c19b63198b452de4fb485ddacbd93d
westy12dan <westy12dan@gmail.com>
2023-04-15 14:20:32 +0800

    Fix lint

--- src/lib/hydrate.ts
+++ src/lib/hydrate.ts
@@ -4,6 +4,7 @@ import { createNode } from '@/lib/createNode.ts';
 if (typeof document !== 'undefined') {
     const component = window.component;
     if (typeof component === 'function') {
+        // deno-lint-ignore no-explicit-any
         const el: any = createNode(document as unknown as HTMLDocument, component({}));
         document.body.replaceChildren(el);
     }
