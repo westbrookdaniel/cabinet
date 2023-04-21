9e12385c4e5d09d49f075b5ab067a81d15b6f739
westy12dan <westy12dan@gmail.com>
2023-04-21 08:13:08 +0800

    Memo examples

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,20 @@
-import { ref } from '@/lib/client.ts';
+import { memo, ref } from '@/lib/client.ts';
 
 export default function Counter() {
-    const count = ref(0);
+    const count1 = ref(0);
+    const count2 = ref(0);
+
+    // Log count1 value when count2 changes
+    memo([count2.value], () => console.log('count1:', count1.value));
 
     return (
         <div>
-            <button onclick={() => count.value--}>-</button>
-            <span style='margin-left: 8px; margin-right: 8px;'>Count: {count.value}</span>
-            <button onclick={() => count.value++}>+</button>
+            <button onclick={() => count1.value--}>-</button>
+            <span style='margin-left: 8px; margin-right: 8px;'>Count1: {count1.value}</span>
+            <button style='margin-right: 16px;' onclick={() => count1.value++}>+</button>
+            <button onclick={() => count2.value--}>-</button>
+            <span style='margin-left: 8px; margin-right: 8px;'>Count2: {count2.value}</span>
+            <button onclick={() => count2.value++}>+</button>
         </div>
     );
 }
